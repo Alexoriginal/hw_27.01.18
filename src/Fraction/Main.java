7df0b534234c35352930bbb49accb06cb3be3e8c
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-01-30 18:41:40 +0200

    rest1

--- src/Fraction/Main.java
+++ src/Fraction/Main.java
@@ -3,7 +3,7 @@ package Fraction;
 public class Main {
     public static void main(String[] args) {
         My_fraction myFrac=new My_fraction(10,5);
-        myFrac.toTheString();
+        System.out.println(myFrac.toTheString());
     }
 
 
