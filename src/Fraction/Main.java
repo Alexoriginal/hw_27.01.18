1b85b6c38ae652175cd0bcce994de084443d1fae
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-01-30 18:29:44 +0200

    rest

--- src/Fraction/Main.java
+++ src/Fraction/Main.java
@@ -2,9 +2,8 @@ package Fraction;
 
 public class Main {
     public static void main(String[] args) {
-        My_fraction myFrac=new My_fraction();
-        myFrac.toSring();
-
+        My_fraction myFrac=new My_fraction(10,5);
+        myFrac.toTheString();
     }
 
 
